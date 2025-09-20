be371afbe03be04e460ab90b458cbf2a849be697
Alexander Nachtmann <anfh22@outlook.com>
2025-09-20 11:26:56 +0200

    Update SseTestHelpers.cs

--- SWEN3.Paperless.RabbitMq.Tests/Helpers/SseTestHelpers.cs
+++ SWEN3.Paperless.RabbitMq.Tests/Helpers/SseTestHelpers.cs
@@ -5,16 +5,22 @@ internal static class SseTestHelpers
     public static TestServer CreateSseTestServer<T>(ISseStream<T> sseStream,
         Action<IEndpointRouteBuilder> configureEndpoints) where T : class
     {
-        var builder = new WebHostBuilder().ConfigureServices(services =>
+        var hostBuilder = new HostBuilder().ConfigureWebHost(webHost =>
         {
-            services.AddSingleton(sseStream);
-            services.AddRouting();
-        }).Configure(app =>
-        {
-            app.UseRouting();
-            app.UseEndpoints(configureEndpoints);
+            webHost.UseTestServer();
+            webHost.ConfigureServices(services =>
+            {
+                services.AddSingleton(sseStream);
+                services.AddRouting();
+            });
+            webHost.Configure(app =>
+            {
+                app.UseRouting();
+                app.UseEndpoints(configureEndpoints);
+            });
         });
 
-        return new TestServer(builder);
+        var host = hostBuilder.StartAsync().GetAwaiter().GetResult();
+        return host.GetTestServer();
     }
 }
